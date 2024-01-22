b55f7e5ab26d42a01acd90c7fa7724f531f76f6d
Martinz <LucaSowza@hotmail.com>
2024-01-22 16:11:51 -0400

    feat: create fetch answer comments and your test

--- src/domain/forum/application/use-cases/fetch-answer-comments.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -1,4 +1,5 @@
 import { Either, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 import { AnswerCommentRepository } from '../repositories/answer-comments-repository'
 
@@ -14,6 +15,7 @@ type FetchAnswerCommentsUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class FetchAnswerCommentsUseCase {
   constructor(private answerCommentRepository: AnswerCommentRepository) {}
 
