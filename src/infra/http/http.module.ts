d6dd5239d9e5e0e33fa95ec11a4fa69cb0784c08
Martinz <LucaSowza@hotmail.com>
2024-01-22 12:44:45 -0400

    fix: lint http.module.ts

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,3 +1,4 @@
+import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question'
@@ -8,6 +9,7 @@ import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/reg
 import { Module } from '@nestjs/common'
 import { CryptographyModule } from '../cryptography/cryptography.module'
 import { DatabaseModule } from '../database/database.module'
+import { AnswerQuestionController } from './controllers/answer-question.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
@@ -15,8 +17,6 @@ import { DeleteQuestionController } from './controllers/delete-question.controll
 import { EditQuestionController } from './controllers/edit-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
-import { AnswerQuestionController } from './controllers/answer-question.controller'
-import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -28,7 +28,7 @@ import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answ
     GetQuestionBySlugController,
     EditQuestionController,
     DeleteQuestionController,
-    AnswerQuestionController
+    AnswerQuestionController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -38,7 +38,7 @@ import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answ
     GetQuestionBySlugUseCase,
     EditQuestionUseCase,
     DeleteQuestionUseCase,
-    AnswerQuestionUseCase
+    AnswerQuestionUseCase,
   ],
 })
 export class HttpModule {}
